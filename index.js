7f332a56eadf73195f0437b37c81f81516429e47
Hossain-Sajjad <sunshinems58.com>
2022-05-08 05:16:47 +0600

    specific item load

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.findOne(query);
+            res.send(result);
+        })
+
     }
     finally { }
 }
