0299d2aefd4cc4a2e1d38ebc976f08c587c66a4f
Hossain-Sajjad <sunshinems58@gmail.com>
2024-04-27 03:51:59 +0600

    vercel issue

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tm2i0ga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = process.env.MONGODB_URI;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
